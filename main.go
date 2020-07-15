3e70acfd80a77810015af49fd9ef6dc947b2b176
Michał K <uyu1@O2.pl>
2020-07-15 15:27:31 +0200

    Add DiscordGo

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+const token string = "NzMyOTQyNDgzODc2NzQxMTIw.Xw79SQ.A6Ja8r6tsK_3VbWfn6u_TeNDxUI"
 
 func main() {
-	fmt.Println("test")
+	discord, err := discordgo.New("Bot " + token)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	err = discord.Open()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Bot is running!")
+
+	<-make(chan struct{})
+	return
 }
